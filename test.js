a4be825342669877edf3bba954c0b1f0da299987
Amir Hossein Narimani <amirhosseinnyn72@gmail.com>
2021-05-29 20:02:44 +0430

    Update test.js

--- test.js
+++ test.js
@@ -2,9 +2,11 @@ import {EmojiButton} from 'https://cdn.jsdelivr.net/npm/@joeattardi/emoji-button
 
 const picker = new EmojiButton({autoHide: false});
 const trigger = document.getElementById('emoji-bar');
+const userStatus = document.getElementById('user-status');
 
 picker.on('emoji', selection => {
     trigger.textContent = selection.emoji;
+    userStatus.textContent = selection.emoji;
     picker.hidePicker();
 });
 
