e4beab7fc86f13f80b948e89fbbf69fa85b90889
Amir Hossein Narimani <amirhosseinnyn72@gmail.com>
2021-06-04 12:53:15 +0430

    Update test.js

--- test.js
+++ test.js
@@ -3,16 +3,6 @@ import {EmojiButton} from 'https://cdn.jsdelivr.net/npm/@joeattardi/emoji-button
 const picker = new EmojiButton({autoHide: false});
 const trigger = document.getElementById('emoji-bar');
 const userStatus = document.getElementById('user-status');
-const
-  range = document.getElementById('range'),
-  rangeV = document.getElementById('rangeV'),
-  setValue = ()=>{
-    const
-      newValue = Number( (range.value - range.min) * 100 / (range.max - range.min) ),
-      newPosition = 10 - (newValue * 0.2);
-    rangeV.innerHTML = `<span>${range.value}</span>`;
-    rangeV.style.left = `calc(${newValue}% + (${newPosition}px))`;
-  };
 
 picker.on('emoji', selection => {
     trigger.textContent = selection.emoji;
@@ -21,6 +11,3 @@ picker.on('emoji', selection => {
 });
 
 trigger.addEventListener('click', () => picker.togglePicker(trigger));
-
-document.addEventListener("DOMContentLoaded", setValue);
-range.addEventListener('input', setValue);
