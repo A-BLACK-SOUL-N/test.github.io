0585b56721ab4a20562ee0a779a9e5afb95271b0
Amir Hossein Narimani <amirhosseinnyn72@gmail.com>
2021-06-09 22:55:30 +0430

    Update test.js

--- test.js
+++ test.js
@@ -20,11 +20,11 @@ function e(e,o,n,i){return new(n||(n=Promise))((function(a,r){function t(e){try{
 const picker = new rn({autoHide: true});
 const trigger = document.getElementById('emoji-trigger');
 const userStatus = document.getElementById('user-status');
-
+// Set emoji containers
 picker.on('emoji', selection => {
     trigger.textContent = selection.emoji;
     userStatus.innerText = selection.emoji;
     picker.hidePicker();
 });
-
+// Handle click on emoji button
 trigger.addEventListener('click', () => picker.togglePicker(trigger));
