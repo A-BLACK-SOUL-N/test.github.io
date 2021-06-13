da805b5513e3c1bb7e7f5712b090c150dc6fcecf
Amir Hossein Narimani <amirhosseinnyn72@gmail.com>
2021-06-13 12:22:54 +0430

    Update test.js

--- test.js
+++ test.js
@@ -1,10 +1,13 @@
 import { EmojiButton } from 'https://cdn.jsdelivr.net/npm/@joeattardi/emoji-button@4.6.0';
-const button = document.querySelector('#emoji-button');
+const button = document.getElementById('emoji-trigger');
+const emojiDemo = document.getElementById('emoji-demo');
 
 const picker = new EmojiButton();
+
 picker.on('emoji', emoji => {
-    console.log('emoji', emoji);
+    emojiDemo.innerText = emoji;
   });
+
 button.addEventListener('click', () => {
   picker.togglePicker(button);
 });
